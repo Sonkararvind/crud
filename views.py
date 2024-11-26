7c2e1ced8ba48f9eaf3518bde088997908865fa4
Arvind-007 <163734439+Sonkararvind@users.noreply.github.com>
2024-11-26 18:12:28 +0530

    views.py

--- views.py
+++ views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
-
+from crud_app.models import Employees
 # Create your views here.
 def INDEX(request):
-    return render(request,'index.html')
+    emp=Employees.objects.all()
+
+    context={
+        'emp':emp,
+    }
+    return render(request,'index.html',context)
+
+def ADD(request):
+ if request.method=='POST':
+     Name=request.POST.get('name')
+     Email=request.POST.get('Email')
+     Address=request.POST.get('Address')
+     Phone=request.POST.get('phone')
+     
+    
+ emp=Employees(
+       name = name,
+      email = email,
+      address = address,
+     phone = phone
+
+  )
+
+ emp.save()
+ return redirect('home')
+
+ return render(request,'add.html')
